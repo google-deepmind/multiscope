dbfc56c7f5f587bff6779f119eece75471057c69
David Szepesvari <dsz@google.com>
2022-09-17 00:13:25 -0600

    Launch a multiscope server, pretend-write text to it.
    Change-Id: I84db313cb449251fa85634aa682345952cbffbe1

---
+++ clients/python/examples/common.py
@@ -0,0 +1,32 @@
+"""This is a module to help to run the examples automatically."""
+
+from typing import Optional, Text
+
+from absl import flags
+
+# from multiscope.remote import clock
+# from remote import group
+
+_STEP_LIMIT = flags.DEFINE_integer("step_limit", None, "Limit on the number of steps.")
+
+
+def step():
+  """step iterates until a maximum number of steps has been reached."""
+  time = 0
+  while _STEP_LIMIT.value is None or time < _STEP_LIMIT.value:
+    yield time
+    time = time + 1
+
+
+# # TODO: untested.
+# class Ticker(clock.Ticker):
+#   """Ticker which returns False when the number of steps has been set and is exhausted."""
+
+#   def __init__(self, name: Text, parent: Optional[group.ParentNode] = None):
+#     super().__init__(name, parent)
+#     self.time = 0
+
+#   def tick(self) -> bool:
+#     super().tick()
+#     self.time += 1
+#     return _STEP_LIMIT.value is None or self.time < _STEP_LIMIT.value
---
+++ clients/python/examples/text.py
@@ -0,0 +1,18 @@
+"""A simple example of using a TextWriter."""
+
+import time
+
+from absl import app
+import multiscope
+from examples import common
+
+
+def main(argv):
+  multiscope.start_server()
+  text = multiscope.TextWriter("Current time")
+  for _ in common.step():
+    text.write(time.strftime("%H:%M:%S %Z on %b %d, %Y"))
+
+
+if __name__ == "__main__":
+  app.run(main)
