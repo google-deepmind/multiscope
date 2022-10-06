b22f9718cac52138dab20b4a2f1ae1bb14eee299
David Szepesvari <dsz@google.com>
2022-10-05 20:53:09 -0600

    Generate the _pb2_grpc python files, not just the _pb2 ones.
    Change-Id: I0628929f698b8233fa913c822423df7fd181b484

--- generate_python_protos.sh
+++ generate_python_protos.sh
@@ -1,12 +1,18 @@
-protoc --proto_path=protos --python_out=clients/python/multiscope/protos \
-	protos/base.proto \
-	protos/root.proto \
-	protos/scalar.proto \
-	protos/table.proto \
-	protos/text.proto \
-	protos/ticker.proto \
-	protos/tree.proto \
-	protos/ui.proto
+# Generates all the python proto files.
+BASE=$( dirname -- "$( readlink -f -- "$0"; )"; )
+cd "$BASE/clients/python"
+pipenv run python -m grpc_tools.protoc -I../../protos \
+  --python_out=multiscope/protos --grpc_python_out=multiscope/protos \
+  ../../protos/base.proto \
+	../../protos/root.proto \
+	../../protos/scalar.proto \
+	../../protos/table.proto \
+	../../protos/text.proto \
+	../../protos/ticker.proto \
+	../../protos/tree.proto \
+	../../protos/ui.proto
+
+echo "Finished."
 
 # TODO(dsz): currently the set of protos need to be declared and maintained
 # in both this and the generate.go files. Unify.
