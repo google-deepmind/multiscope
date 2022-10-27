1aa7c433cdc25aa2e5f5cee7411a68d133ee7757
Thomas Degris <degris@google.com>
2022-10-27 16:20:51 +0100

    Add a setter to set the url of the WASM file.
    Change-Id: Icd9fcdb4fb100e26c0a1444560613ade068bb4b7

--- internal/server/wasmurl/wasmurl.go
+++ internal/server/wasmurl/wasmurl.go
@@ -3,6 +3,11 @@ package wasmurl
 
 var url = "/res/multiscope.wasm"
 
+// Set the URL for the WASM file.
+func Set(u string) {
+	url = u
+}
+
 // URL returns the WASM url.
 func URL() string {
 	return url
