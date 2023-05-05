775e3ac0a2a5a5104ff86ed81368f7fcc8cb359c
Thomas Degris <degris@google.com>
2023-05-05 11:31:58 +0100

    Copy wasm_exec.js.
    Change-Id: Id0699e38084f7c7a4abaccec2357c2e8b6cc41a7

--- generate.go
+++ generate.go
@@ -1,8 +1,7 @@
 // Package multiscope is used to generate all the files require for Multiscope.
 package multiscope
 
-//#go:generate cp $GOROOT/misc/wasm/wasm_exec.js ./web/res
-
 // Generate web-assembly client.
 
+//go:generate cp $GOROOT/misc/wasm/wasm_exec.js ./web/res
 //go:generate zsh -c "GOOS=js GOARCH=wasm go build -o web/res/multiscope.wasm wasm/mains/multiscope/main.go && gzip -9 -f web/res/multiscope.wasm"
