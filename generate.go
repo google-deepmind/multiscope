67d9021d0057a5d3ae38b7c2b196b7dae9154ac1
Thomas Degris <degris@google.com>
2023-05-09 14:17:10 +0100

    Use Go provided by GOROOT to compile the wasm.
    Change-Id: I51a5940ff1da948e9eb89e6929317a770afc93f4

--- generate.go
+++ generate.go
@@ -4,4 +4,4 @@ package multiscope
 // Generate web-assembly client.
 
 //go:generate cp $GOROOT/misc/wasm/wasm_exec.js ./web/res
-//go:generate zsh -c "GOOS=js GOARCH=wasm go build -o web/res/multiscope.wasm wasm/mains/multiscope/main.go && gzip -9 -f web/res/multiscope.wasm"
+//go:generate zsh -c "GOOS=js GOARCH=wasm $GOROOT/bin/go build -o web/res/multiscope.wasm wasm/mains/multiscope/main.go && gzip -9 -f web/res/multiscope.wasm"
