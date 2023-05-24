48d10a51157eb28508a62a28126cdaf26f31892b
Thomas Degris <degris@google.com>
2023-05-24 15:10:17 +0100

    Change the license to Apache 2.
    Change-Id: Ifc54dfc84eca53775bc4710d64fdc98981dc83cb

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -1,5 +1,18 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # pylint: disable=g-bad-file-header
-# TODO(dsz): add copyright and license info.
 
 """Install script for setuptools."""
 
@@ -16,13 +29,7 @@ setup(
     keywords="analysis visualization python reinforcement-learning machine learning",
     packages=find_packages(exclude=["examples"]),
     python_requires=">=3.7, <4",
-    install_requires=[
-        # TODO(dsz): fill out later.
-    ],
-    tests_require=[
-        # TODO(dsz): fill out later.
-    ],
-    classifiers=[
-        # TODO(dsz): fill out later.
-    ],
+    install_requires=[],
+    tests_require=[],
+    classifiers=[],
 )
